ccea6153d4939fbde380fca309c60a7d1ceb7001
Rodrigo <rpngithub001@gmail.com>
2026-08-24 14:43:23 -0300

    feat: cria microservico auth_service

---
+++ auth-service/src/test/java/com/miniTaskManager/auth_service/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniTaskManager.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
