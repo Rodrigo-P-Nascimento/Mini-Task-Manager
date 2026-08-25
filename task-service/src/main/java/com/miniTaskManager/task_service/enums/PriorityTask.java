0bf6c8d4adb5f63c8613ee5cd5f1a548a4e7f2e2
Rodrigo <rpngithub001@gmail.com>
2026-08-25 09:21:07 -0300

    feat: cria enums task service

---
+++ task-service/src/main/java/com/miniTaskManager/task_service/enums/PriorityTask.java
@@ -0,0 +1,7 @@
+package main.java.com.miniTaskManager.task_service.enums;
+
+public enum PriorityTask {
+    BAIXA,
+    MEDIA,
+    ALTA
+}
---
+++ task-service/src/main/java/com/miniTaskManager/task_service/enums/StatusTask.java
@@ -0,0 +1,7 @@
+package main.java.com.miniTaskManager.task_service.enums;
+
+public enum StatusTask {
+    PENDENTE,
+    EM_ANDAMENTO,
+    CONCLUIDA
+}
